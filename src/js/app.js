7d2d28f998810d1a366ea1774acf04e873266a2c
acrysense <54480354+acrysense@users.noreply.github.com>
2021-05-30 12:03:47 +0300

    Improvements #1

--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // MODAL
     const modalBtn = document.querySelectorAll('.modal-btn'),
+        modalMaterialBtn = document.querySelectorAll('.modal-material-btn'),
         modal = document.querySelectorAll('.modal'),
-        modalOverlay = document.querySelector('.modal-overlay')
+        modalMaterial = document.querySelector('.modal-material'),
+        modalOverlay = document.querySelector('.modal-overlay'),
+        modalMaterialClose = document.querySelector('.modal-material__close')
     
     if (modalBtn) {
         modalBtn.forEach((item, i) => {
@@ -28,14 +31,31 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    if (modalMaterialBtn) {
+        modalMaterialBtn.forEach((item) => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault();
+
+                if (!modalOverlay.classList.contains('modal-overlay--active')) {
+                    modalOverlay.classList.add('modal-overlay--active')
+                }
+                document.body.classList.add('scroll-disabled');
+                modalMaterial.classList.add('modal-material--active');
+            })
+        })
+    }
+
     document.body.addEventListener('keyup', (event) => {
         let key = event.keyCode;
 
-        if (modal && modalOverlay.classList.contains('modal-overlay--active')) {
+        if (modal || modalMaterial && modalOverlay.classList.contains('modal-overlay--active')) {
             if (key == 27) {
                 document.body.classList.remove('scroll-disabled');
                 document.querySelectorAll('.modal.modal--active').forEach((child) => child.classList.remove('modal--active'));
-                document.querySelector('.modal-overlay').classList.remove('modal-overlay--active');
+                if (modalMaterial && modalMaterial.classList.contains('modal-material--active')) {
+                    modalMaterial.classList.remove('modal-material--active');
+                }
+                modalOverlay.classList.remove('modal-overlay--active');
             };
         }
     }, false);
@@ -43,11 +63,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (modalOverlay) {
         modalOverlay.addEventListener('click', () => {
-            if (modal && modalOverlay.classList.contains('modal-overlay--active')) {
+            if (modal || modalMaterial && modalOverlay.classList.contains('modal-overlay--active')) {
                 document.body.classList.remove('scroll-disabled');
                 document.querySelectorAll('.modal.modal--active').forEach((child) => child.classList.remove('modal--active'));
-                document.querySelector('.modal-overlay').classList.remove('modal-overlay--active');
+                if (modalMaterial && modalMaterial.classList.contains('modal-material--active')) {
+                    modalMaterial.classList.remove('modal-material--active');
+                }
+                modalOverlay.classList.remove('modal-overlay--active');
             }
         });
+    }
+
+    if (modalMaterialClose) {
+        modalMaterialClose.addEventListener('click', () => {
+            if (modalMaterial && modalOverlay.classList.contains('modal-overlay--active')) {
+                document.body.classList.remove('scroll-disabled');
+                modalMaterial.classList.remove('modal-material--active');
+                modalOverlay.classList.remove('modal-overlay--active');
+            }
+        })
+    }
+
+    // HEIGHT 100VH FIX FOR IOS
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+    // MODAL MATERIAL MOBILE
+    const modalMaterialTrigger = document.querySelectorAll('.modal-material__title')
+
+    if (modalMaterialTrigger) {
+        modalMaterialTrigger.forEach((item) => {
+            item.addEventListener('click', () => {
+                item.parentNode.classList.toggle('modal-material__column--hidden')
+            })
+        })
     }
 });
